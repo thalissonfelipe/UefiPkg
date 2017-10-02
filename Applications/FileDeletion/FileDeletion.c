2ea96d97c4b9825ae256d0fdac5412d88e9e0677
Thalisson <thalissonlipe7@gmail.com>
2017-10-02 17:32:12 -0300

    finalizado a aplicação de deletar arquivos

--- Applications/FileDeletion/FileDeletion.c
+++ Applications/FileDeletion/FileDeletion.c
@@ -14,7 +14,9 @@ UefiMain (
 )
 {
     EFI_STATUS Status = EFI_SUCCESS;
-    EFI_HANDLE NtfsHandle = NULL;
+    EFI_HANDLE *NtfsHandle = NULL;
+    UINTN NoHandles;
+    UINTN i;
     UINTN BufferSize = SIZE_OF_EFI_FILE_INFO + 512 * sizeof(CHAR16);
     EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *FileSystem = NULL;
     EFI_FILE_INFO *FileInfo = NULL;
@@ -23,106 +25,153 @@ UefiMain (
     EFI_FILE_PROTOCOL *File = NULL;
     EFI_FILE_PROTOCOL *FileAux = NULL;
 
-    Status = gBS->LocateHandle(
+    Status = gBS->LocateHandleBuffer(
         ByProtocol,
         &gEfiSimpleFileSystemProtocolGuid,
         NULL,
-        &BufferSize,
+        &NoHandles,
         &NtfsHandle);
-    if (Status == EFI_BUFFER_TOO_SMALL) {
-        Print(L"Required handle buffer of size: %d\n", BufferSize);
-    } else if (EFI_ERROR(Status)) {
-        Print(L"Could not find any NTFS volume: %r\n", Status);
+    if (EFI_ERROR(Status)) {
+        Print(L"Could not find any handles: %r\n", Status);
         goto bottom;
     }
-    Print(L"Found a NTFS file system\n");
+    Print(L"%d handle(s) encontrado(s)\n", NoHandles);
 
-    Status = gBS->OpenProtocol(
-        NtfsHandle,
-        &gEfiSimpleFileSystemProtocolGuid,
-        (VOID**) &FileSystem,
-        ImageHandle,
-        NULL,
-        EFI_OPEN_PROTOCOL_GET_PROTOCOL);
-    if (EFI_ERROR(Status)) {
-        Print(L"Could not open NTFS sile system protocol: %r\n", Status);
-        goto bottom;
-    }
-
-    Status = FileSystem->OpenVolume(
-        FileSystem,
-        &RootDir);
-    if (EFI_ERROR(Status)) {
-        Print(L"Could not open root dir: %r\n", Status);
-        goto close_fs;
-    }
-
-    Status = RootDir->Open(
-            RootDir,
-            &File,
-            L"files.conf",
-            EFI_FILE_MODE_READ,
-            0);
-    if (EFI_ERROR(Status)) {
-        Print(L"Could not open file: %r\n", Status);
-        goto close_root;
-    }
-
-    BufferSize = SIZE_OF_EFI_FILE_INFO + 512 * sizeof(CHAR16);
-    Status = File->GetInfo(
-    		File,
-			&gEfiFileInfoGuid,
-			&BufferSize,
-			FileInfo);
-    if (EFI_ERROR(Status)) {
-    	Print(L"Could not get info: %r\n", Status);
-    	goto close_file;
-    }
-    Print(L"FileSize: %d\n", FileInfo->FileSize);
-
-    CHAR8 BarraN = '\n';
-    CHAR8 MiniBuffer[2];
-    UINTN Size = 0;
-    UINTN Count = 0;
-    CHAR16 *Buffer = AllocateZeroPool(512 * sizeof(CHAR16));
-    CHAR16 *BufferAux;
-
-    while (Count <= FileInfo->FileSize - 1) {
-		BufferSize = 1;
-		Status = File->Read(File, &BufferSize, MiniBuffer);
+    for (i = 0; i < NoHandles; i++) {
+		Status = gBS->OpenProtocol(
+			NtfsHandle[i],
+			&gEfiSimpleFileSystemProtocolGuid,
+			(VOID**) &FileSystem,
+			ImageHandle,
+			NULL,
+			EFI_OPEN_PROTOCOL_GET_PROTOCOL);
 		if (EFI_ERROR(Status)) {
-			Print(L"Could not read file: %r\n", Status);
-			goto close_file;
+			Print(L"Could not open system protocol: %r\n", Status);
+			goto bottom;
 		}
 
-		MiniBuffer[1] = '\0';
-		BufferAux = AllocateZeroPool((AsciiStrLen(MiniBuffer) + 1) * sizeof(CHAR16));
-		AsciiStrToUnicodeStr(MiniBuffer, BufferAux);
-		StrCat(Buffer, BufferAux);
+		Status = FileSystem->OpenVolume(
+			FileSystem,
+			&RootDir);
+		if (EFI_ERROR(Status)) {
+			Print(L"Could not open root dir: %r\n", Status);
+			goto close_fs;
+		}
 
-		if(BarraN == MiniBuffer[0]) {
-			Size = StrLen(Buffer) - 1;
-			Buffer[Size] = '\0';
-			Print(L"%Buffer: %s\n", Buffer);
-			Status = RootDir->Open(
-					RootDir,
-					&FileAux,
-					Buffer,
-					EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE,
-					0);
+		Status = RootDir->Open(
+				RootDir,
+				&File,
+				L".",
+				EFI_FILE_MODE_READ,
+				0);
+		if (EFI_ERROR(Status)) {
+			Print(L"Could not open file: %r\n", Status);
+			goto close_root;
+		}
+
+		UINTN Count = 0;
+		while (TRUE) {
+			BufferSize = SIZE_OF_EFI_FILE_INFO + 512 * sizeof(CHAR16);
+			Status = File->Read(File, &BufferSize, FileInfo);
 			if (EFI_ERROR(Status)) {
-				Print(L"Could not open file: %r\n", Status);
-				goto close_root;
-			}
-			Status = FileAux->Delete(FileAux);
-			if (EFI_ERROR(Status)) {
-				Print(L"Could not delete file: %r\n", Status);
+				Print(L"Could not read file: %r\n", Status);
 				goto close_file;
 			}
-			Print(L"Arquivo (%s) deletado com sucesso!\n", Buffer);
-			Buffer = AllocateZeroPool(512 * sizeof(CHAR16));
+			if (BufferSize == 0) {
+				goto end_for;
+			}
+			if (StrCmp(FileInfo->FileName, L"root_deletion") == 0)
+				Count++;
+			if (StrCmp(FileInfo->FileName, L"file_deletion.conf") == 0)
+				Count++;
+			if (Count == 2) {
+				Print(L"Tudo certo!\n");
+				i = NoHandles;
+				break;
+			}
 		}
-		Count++;
+
+		Status = RootDir->Open(
+				RootDir,
+				&File,
+				L"file_deletion.conf",
+				EFI_FILE_MODE_READ,
+				0);
+		if (EFI_ERROR(Status)) {
+			Print(L"Could not open file_deletion.conf: %r\n", Status);
+			goto close_root;
+		}
+
+		Status = RootDir->Open(
+				RootDir,
+				&RootDir,
+				L"root_deletion",
+				EFI_FILE_MODE_READ,
+				0);
+		if (EFI_ERROR(Status)) {
+			Print(L"Could not open root_deletion: %r\n", Status);
+			goto close_root;
+		}
+
+		BufferSize = SIZE_OF_EFI_FILE_INFO + 512 * sizeof(CHAR16);
+		Status = File->GetInfo(
+				File,
+				&gEfiFileInfoGuid,
+				&BufferSize,
+				FileInfo);
+		if (EFI_ERROR(Status)) {
+			Print(L"Could not get info: %r\n", Status);
+			goto close_file;
+		}
+		Print(L"FileSize: %d\n", FileInfo->FileSize);
+
+		CHAR8 MiniBuffer[2];
+		UINTN StringSize = 0;
+		Count = 0;
+		CHAR16 *Buffer = AllocateZeroPool(512 * sizeof(CHAR16));
+		CHAR16 *BufferAux;
+
+		while (Count <= FileInfo->FileSize - 1) {
+			BufferSize = 1;
+			Status = File->Read(File, &BufferSize, MiniBuffer);
+			if (EFI_ERROR(Status)) {
+				Print(L"Could not read file: %r\n", Status);
+				goto close_file;
+			}
+
+			MiniBuffer[1] = '\0';
+			BufferAux = AllocateZeroPool((AsciiStrLen(MiniBuffer) + 1) * sizeof(CHAR16));
+			AsciiStrToUnicodeStr(MiniBuffer, BufferAux);
+			StrCat(Buffer, BufferAux);
+
+			if(MiniBuffer[0] == '\n') {
+				StringSize = StrLen(Buffer) - 1;
+				Buffer[StringSize] = '\0';
+				Print(L"%Buffer: %s\n", Buffer);
+				Status = RootDir->Open(
+						RootDir,
+						&FileAux,
+						Buffer,
+						EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE,
+						0);
+				if (EFI_ERROR(Status)) {
+					Print(L"Could not open file: %r\n", Status);
+					goto close_root;
+				}
+				Status = FileAux->Delete(FileAux);
+				if (EFI_ERROR(Status)) {
+					Print(L"Could not delete file: %r\n", Status);
+					goto close_file;
+				}
+				Print(L"Arquivo (%s) deletado com sucesso!\n", Buffer);
+				Buffer = AllocateZeroPool(512 * sizeof(CHAR16));
+			}
+			Count++;
+		}
+
+		end_for:
+			continue;
+
     }
 
 close_file:
