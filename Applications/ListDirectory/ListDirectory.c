e6bd415a9b4e4b422d1da3c5c189c6bb0eb01746
Thalisson <thalissonlipe7@gmail.com>
2018-02-20 16:07:28 -0300

    Updating ListDirectory application.

--- Applications/ListDirectory/ListDirectory.c
+++ Applications/ListDirectory/ListDirectory.c
@@ -1,10 +1,10 @@
-#include  <Uefi.h>
-#include  <Library/UefiLib.h>
-#include  <Library/UefiApplicationEntryPoint.h>
-#include  <Library/UefiBootServicesTableLib.h>
-#include  <Library/MemoryAllocationLib.h>
-#include  <Protocol/SimpleFileSystem.h>
-#include  <Guid/FileInfo.h>
+#include <Uefi.h>
+#include <Library/UefiLib.h>
+#include <Library/UefiApplicationEntryPoint.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Protocol/SimpleFileSystem.h>
+#include <FileSystemLib/FileSystemLib.h>
+#include <Guid/FileInfo.h>
 
 EFI_STATUS
 EFIAPI
@@ -14,46 +14,15 @@ UefiMain (
 )
 {
     EFI_STATUS Status = EFI_SUCCESS;
-    EFI_HANDLE NtfsHandle = NULL;
     UINTN BufferSize = SIZE_OF_EFI_FILE_INFO + 512 * sizeof(CHAR16);
-    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *FileSystem = NULL;
     EFI_FILE_INFO *FileInfo = NULL;
-    FileInfo = (EFI_FILE_INFO*)AllocateZeroPool(BufferSize);
     EFI_FILE_PROTOCOL *RootDir = NULL;
     EFI_FILE_PROTOCOL *File = NULL;
 
-    Status = gBS->LocateHandle(
-        ByProtocol,
-        &gEfiSimpleFileSystemProtocolGuid,
-        NULL,
-        &BufferSize,
-        &NtfsHandle);
-    if (Status == EFI_BUFFER_TOO_SMALL) {
-        Print(L"Required handle buffer of size: %d\n", BufferSize);
-    } else if (EFI_ERROR(Status)) {
-        Print(L"Could not find any NTFS volume: %r\n", Status);
-        goto bottom;
-    }
-    Print(L"Found a NTFS file system\n");
-
-    Status = gBS->OpenProtocol(
-        NtfsHandle,
-        &gEfiSimpleFileSystemProtocolGuid,
-        (VOID**) &FileSystem,
-        ImageHandle,
-        NULL,
-        EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+    Status = OpenRootDir(ImageHandle, &RootDir);
     if (EFI_ERROR(Status)) {
-        Print(L"Could not open NTFS sile system protocol: %r\n", Status);
-        goto bottom;
-    }
-
-    Status = FileSystem->OpenVolume(
-        FileSystem,
-        &RootDir);
-    if (EFI_ERROR(Status)) {
-        Print(L"Could not open root dir: %r\n", Status);
-        goto close_fs;
+    	Print(L"Could not open root dir: %r\n", Status);
+    	return Status;
     }
 
     Status = RootDir->Open(
@@ -64,38 +33,34 @@ UefiMain (
             0);
     if (EFI_ERROR(Status)) {
         Print(L"Could not open file: %r\n", Status);
-        goto close_root;
+        goto FREE_RESOURCES;
     }
 
+    FileInfo = AllocateZeroPool(BufferSize);
     while (TRUE) {
-    	BufferSize = SIZE_OF_EFI_FILE_INFO + 512 * sizeof(CHAR16);
 		Status = File->Read(File, &BufferSize, FileInfo);
 		if (EFI_ERROR(Status)) {
 			Print(L"Could not read file: %r\n", Status);
-			goto close_file;
+			goto FREE_RESOURCES;
 		}
 		if (BufferSize == 0) {
-			goto close_file;
+			goto FREE_RESOURCES;
 		}
-		Print(L"File: %s\n", FileInfo->FileName);
+		Print(L"%s\n", FileInfo->FileName);
+		BufferSize = SIZE_OF_EFI_FILE_INFO + 512 * sizeof(CHAR16);
     }
 
-    FreePool(FileInfo);
+FREE_RESOURCES:
 
+	if (FileInfo != NULL) {
+    	FreePool(FileInfo);
+    }
 
-close_file:
-    File->Close(File);
+    Status = CloseFileProtocol(File);
 
-close_root:
-    RootDir->Close(RootDir);
+    Status = CloseFileProtocol(RootDir);
 
-close_fs:
-    gBS->CloseProtocol(
-        NtfsHandle,
-        &gEfiSimpleFileSystemProtocolGuid,
-        ImageHandle,
-        NULL);
+    Status = CloseProtocol();
 
-bottom:
     return Status;
 }
