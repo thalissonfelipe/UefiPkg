f3576ca7eb296b2bd7a665617e13e5f35b970eb4
Thalisson <thalissonlipe7@gmail.com>
2018-02-22 12:55:22 -0300

    Updating FileDeletion application.

--- Applications/FileDeletion/FileDeletion.c
+++ Applications/FileDeletion/FileDeletion.c
@@ -1,10 +1,10 @@
-#include  <Uefi.h>
-#include  <Library/UefiLib.h>
-#include  <Library/UefiApplicationEntryPoint.h>
-#include  <Library/UefiBootServicesTableLib.h>
-#include  <Library/MemoryAllocationLib.h>
-#include  <Protocol/SimpleFileSystem.h>
-#include  <Guid/FileInfo.h>
+#include <Uefi.h>
+#include <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Protocol/SimpleFileSystem.h>
+#include <Guid/FileInfo.h>
+#include <FileSystemLib/FileSystemLib.h>
 
 EFI_STATUS
 EFIAPI
@@ -14,32 +14,35 @@ UefiMain (
 )
 {
     EFI_STATUS Status = EFI_SUCCESS;
-    EFI_HANDLE *NtfsHandle = NULL;
+    EFI_HANDLE *Handle = NULL;
     UINTN NoHandles;
-    UINTN i;
-    UINTN BufferSize = SIZE_OF_EFI_FILE_INFO + 512 * sizeof(CHAR16);
+    UINTN Index;
+    UINTN BufferSize;
+    UINTN Count = 0;
     EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *FileSystem = NULL;
-    EFI_FILE_INFO *FileInfo = NULL;
-    FileInfo = (EFI_FILE_INFO*)AllocateZeroPool(BufferSize);
     EFI_FILE_PROTOCOL *RootDir = NULL;
     EFI_FILE_PROTOCOL *File = NULL;
     EFI_FILE_PROTOCOL *FileAux = NULL;
+    EFI_FILE_INFO *FileInfo = NULL;
+    CHAR8 MiniBuffer[2];
+	CHAR16 *Buffer = NULL;
+	CHAR16 *BufferAux = NULL;
 
     Status = gBS->LocateHandleBuffer(
         ByProtocol,
         &gEfiSimpleFileSystemProtocolGuid,
         NULL,
         &NoHandles,
-        &NtfsHandle);
+        &Handle);
     if (EFI_ERROR(Status)) {
         Print(L"Could not find any handles: %r\n", Status);
-        goto bottom;
+        return Status;
     }
-    Print(L"%d handle(s) encontrado(s)\n", NoHandles);
+    Print(L"%d handle(s) found.\n", NoHandles);
 
-    for (i = 0; i < NoHandles; i++) {
+    for (Index = 0; Index < NoHandles; Index++) {
 		Status = gBS->OpenProtocol(
-			NtfsHandle[i],
+			Handle[Index],
 			&gEfiSimpleFileSystemProtocolGuid,
 			(VOID**) &FileSystem,
 			ImageHandle,
@@ -47,14 +50,12 @@ UefiMain (
 			EFI_OPEN_PROTOCOL_GET_PROTOCOL);
 		if (EFI_ERROR(Status)) {
 			Print(L"Could not open system protocol: %r\n", Status);
-			goto bottom;
+			return Status;
 		}
 
-		Status = FileSystem->OpenVolume(
-			FileSystem,
-			&RootDir);
+		Status = FileSystem->OpenVolume(FileSystem, &RootDir);
 		if (EFI_ERROR(Status)) {
-			Print(L"Could not open root dir: %r\n", Status);
+			Print(L"Could not open volume: %r\n", Status);
 			goto close_fs;
 		}
 
@@ -65,39 +66,8 @@ UefiMain (
 				EFI_FILE_MODE_READ,
 				0);
 		if (EFI_ERROR(Status)) {
-			Print(L"Could not open file: %r\n", Status);
-			goto close_root;
-		}
-
-		UINTN Count = 0;
-		while (TRUE) {
-			BufferSize = SIZE_OF_EFI_FILE_INFO + 512 * sizeof(CHAR16);
-			Status = File->Read(File, &BufferSize, FileInfo);
-			if (EFI_ERROR(Status)) {
-				Print(L"Could not read file: %r\n", Status);
-				goto close_file;
-			}
-			if (BufferSize == 0) {
-				goto end_for;
-			}
-			if (StrCmp(FileInfo->FileName, L"root_deletion") == 0)
-				Count++;
-			if (StrCmp(FileInfo->FileName, L"file_deletion.conf") == 0)
-				Count++;
-			if (Count == 2) {
-				break;
-			}
-		}
-
-		Status = RootDir->Open(
-				RootDir,
-				&File,
-				L"file_deletion.conf",
-				EFI_FILE_MODE_READ,
-				0);
-		if (EFI_ERROR(Status)) {
-			Print(L"Could not open file_deletion.conf: %r\n", Status);
-			goto close_root;
+			Print(L"Could not open root dir: %r\n", Status);
+			goto FREE_RESOURCES;
 		}
 
 		Status = RootDir->Open(
@@ -108,10 +78,24 @@ UefiMain (
 				0);
 		if (EFI_ERROR(Status)) {
 			Print(L"Could not open root_deletion: %r\n", Status);
-			goto close_root;
+			continue;
 		}
 
+		Status = RootDir->Open(
+				RootDir,
+				&File,
+				L"file_deletion.conf",
+				EFI_FILE_MODE_READ,
+				0);
+		if (EFI_ERROR(Status)) {
+			Print(L"Could not open file_deletion.conf: %r\n", Status);
+			continue;
+		}
+
+		Index = NoHandles;
+
 		BufferSize = SIZE_OF_EFI_FILE_INFO + 512 * sizeof(CHAR16);
+		FileInfo = AllocateZeroPool(BufferSize);
 		Status = File->GetInfo(
 				File,
 				&gEfiFileInfoGuid,
@@ -119,22 +103,18 @@ UefiMain (
 				FileInfo);
 		if (EFI_ERROR(Status)) {
 			Print(L"Could not get info: %r\n", Status);
-			goto close_file;
+			goto FREE_RESOURCES;
 		}
-		Print(L"FileSize: %d\n", FileInfo->FileSize);
 
-		CHAR8 MiniBuffer[2];
-		UINTN StringSize = 0;
-		Count = 0;
-		CHAR16 *Buffer = AllocateZeroPool(512 * sizeof(CHAR16));
-		CHAR16 *BufferAux;
+		Buffer = AllocateZeroPool(512 * sizeof(CHAR16));
 
 		while (Count <= FileInfo->FileSize - 1) {
 			BufferSize = 1;
+
 			Status = File->Read(File, &BufferSize, MiniBuffer);
 			if (EFI_ERROR(Status)) {
 				Print(L"Could not read file: %r\n", Status);
-				goto close_file;
+				goto FREE_RESOURCES;
 			}
 
 			MiniBuffer[1] = '\0';
@@ -143,9 +123,7 @@ UefiMain (
 			StrCat(Buffer, BufferAux);
 
 			if(MiniBuffer[0] == '\n') {
-				StringSize = StrLen(Buffer) - 1;
-				Buffer[StringSize] = '\0';
-				Print(L"%Buffer: %s\n", Buffer);
+				Buffer[StrLen(Buffer) - 1] = '\0';
 				Status = RootDir->Open(
 						RootDir,
 						&FileAux,
@@ -154,47 +132,46 @@ UefiMain (
 						0);
 				if (EFI_ERROR(Status)) {
 					Print(L"Could not open file: %r\n", Status);
-					goto close_root;
+					goto FREE_RESOURCES;
 				}
+
 				Status = FileAux->Delete(FileAux);
 				if (EFI_ERROR(Status)) {
 					Print(L"Could not delete file: %r\n", Status);
-					goto close_file;
+					goto FREE_RESOURCES;
 				}
-				Print(L"Arquivo %s deletado com sucesso!\n", Buffer);
+
+				Print(L"%s deleted successfully.\n", Buffer);
 				Buffer[0] = '\0';
 			}
 			Count++;
 		}
-
-		FreePool(Buffer);
-		FreePool(BufferAux);
-		FreePool(FileInfo);
-
-		goto close_file;
-
-		end_for:
-			continue;
-
     }
 
-    Print(L"Sistema de arquivos não encontrado.\n");
+FREE_RESOURCES:
 
+	if (FileInfo != NULL) {
+		FreePool(FileInfo);
+	}
 
+	if (Buffer != NULL) {
+		FreePool(Buffer);
+	}
 
-close_file:
-    File->Close(File);
+	if (BufferAux != NULL) {
+		FreePool(BufferAux);
+	}
 
-close_root:
-    RootDir->Close(RootDir);
+	Status = CloseFileProtocol(File);
+
+    Status = CloseFileProtocol(RootDir);
 
 close_fs:
-    gBS->CloseProtocol(
-        NtfsHandle,
+    Status = gBS->CloseProtocol(
+        Handle,
         &gEfiSimpleFileSystemProtocolGuid,
         ImageHandle,
         NULL);
 
-bottom:
     return Status;
 }
